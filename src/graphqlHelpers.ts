aef38751ce918b45b570a55af14d9edd6fb71903
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-06 21:37:41 -0700

    Fix codegen issues (#4)
    * Fix codegen issues
    
    * dispatch on JSONObject instead

--- src/graphqlHelpers.ts
+++ src/graphqlHelpers.ts
@@ -97,7 +97,7 @@ const scalarMap: Record<string, OutType> = {
   Boolean: { kind: "scalar", type: "boolean" },
   GitHubGitObjectID: { kind: "scalar", type: "string" },
   GitHubURI: { kind: "scalar", type: "string" },
-  // JSON: "JSON",
+  JSONObject: { kind: "scalar", type: "Record<string, unknown>" },
 };
 
 export function gatherAllReferencedTypes(
@@ -156,6 +156,7 @@ export function typeScriptForGraphQLType(
     Boolean: "boolean",
     GitHubURI: "string",
     GitHubTimestamp: "string",
+    JSONObject: "Record<string, unknown>",
   };
 
   if (isListType(gqlType)) {
